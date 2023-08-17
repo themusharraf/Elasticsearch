353be66fe1e0dc79f1aec8a5476446f2883e2a3a
msi <meibrohimov@gmail.com>
2023-08-17 21:40:12 +0500

    add new py file 😎

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django_elasticsearch_dsl_drf.constants import SUGGESTER_COMPLETION
+from django_elasticsearch_dsl_drf.filter_backends import SearchFilterBackend, FilteringFilterBackend, \
+    SuggesterFilterBackend
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from apps.documents import ArticleDocument
+from apps.serializers import ArticleDocumentSerializer
 
-# Create your views here.
+
+class ArticleDocumentView(DocumentViewSet):
+    document = ArticleDocument
+    serializer_class = ArticleDocumentSerializer
+
+    filter_backends = [
+        FilteringFilterBackend,
+        SearchFilterBackend,
+        SuggesterFilterBackend
+    ]
+
+    search_fields = (
+        'title',
+    )
+
+    filter_fields = {
+        'category': 'category.id'
+    }
+
+    suggester_fields = {
+        'title': {
+            'field': 'title.suggest',
+            'suggesters': [
+                SUGGESTER_COMPLETION,
+            ],
+        },
--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+from apps.views import ArticleDocumentView
+
+router = routers.SimpleRouter(trailing_slash=False)
+
+router.register(r'article-search', ArticleDocumentView, basename='article-search')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
